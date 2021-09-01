f6c21d90dfccbe69f679ec9210404aa07777b638
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-09-01 17:22:38 +0300

    first class

---
+++ src/testDog.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class testDog {
+    public static void main (String[] args) {
+        Dog d1 = new Dog("Mike", 2);
+        Dog d2 = new Dog("Helen", 7);
+        Dog d3 = new Dog("Bob");
+        d3.setAge(1);
+        System.out.println(d1);
+        d1.intoHumanAge();
+        d2.intoHumanAge();
+        d3.intoHumanAge();
+    }
+}
